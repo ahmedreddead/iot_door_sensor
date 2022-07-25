4aa111409b97cd1b91de6e0e6b9eadfa9050d3d6
ahmed mohamed <45078408+ahmedreddead@users.noreply.github.com>
2022-07-25 11:34:14 +0400

    Update mqtt_door_sensor.py

--- mqtt_door_sensor.py
+++ mqtt_door_sensor.py
@@ -1,3 +1,5 @@
+import json
+
 import paho.mqtt.client as mqtt
 import time
 
@@ -20,20 +22,19 @@ def hub_to_mqtt(st) :
         status = 0
     battery = 3.3
     online = 1
-    payload = '\n\"serial_number\":\"{}\"\n , \n\"battery\":{}\n , \n\"state\":{}\n , \n\"online\":{}\n'.format(
-        serial_number, battery, status, online)
-    payload = '{' + payload + '}' + '\n'
-    print(payload)
-    client.publish(topic_sensor_state, payload)
+
+    json_payload_dictionary = {"serial_number": serial_number, "battery": battery, "state": status, "online": online}
+    json_payload = json.dumps(json_payload_dictionary)
+
+    client.publish(topic_sensor_state,str(json_payload))
     ####### alarm
     if alarm_bool :
         client.subscribe(topic_alarm)
         id = "12F34"
         list_of_sensors = [serial_number]
-        payload = '\n\"id\":\"{}\"\n,\n\"sensors\":{}\n'.format(
-            id, list_of_sensors)
-        payload = '{' + payload + '}' + '\n'
-        client.publish(topic_alarm, payload)
+        json_payload_dictionary = {"id": id, "sensors": list_of_sensors}
+        json_payload = json.dumps(json_payload_dictionary)
+        client.publish(topic_alarm, str(json_payload))
 
 def on_message(client, userdata, message):
     print("received message: " ,str(message.payload.decode("utf-8")))
@@ -48,5 +49,5 @@ client.loop_start()
 client.subscribe("Micropolis/door_sensor/")
 client.on_message=on_message
 while True :
-    time.sleep(10)
+    time.sleep(500)
 #client.loop_stop()
